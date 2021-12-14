42715db7ac0839e123efdcaa5918f4fe56e54fdb
Leandro Lopez <lopezlh982@gmail.com>
2021-12-14 20:23:26 -0300

    actividades de la clase 4

--- clase-4/JS/index.js
+++ clase-4/JS/index.js
@@ -264,15 +264,78 @@
 //funciones flecha
 
 //Funcion por nombre
-function calcPrecioTotal(precio) {
-    let impuesto = 1.20;
-    let envio = 20;
-    let precioTotal = (precio * impuesto ) + envio;
+// function calcPrecioTotal(precio) {
+//     let impuesto = 1.20;
+//     let envio = 20;
+//     let precioTotal = (precio * impuesto ) + envio;
+// }
+
+// //funcion por flecha
+// const calcPrecioTotal = precio => {
+//     let impuesto = 1.20;
+//     let envio = 20;
+//     let precioTotal = (precio * impuesto ) + envio;
+// }
+
+//======================================================================
+
+/**
+ * actividades practicas
+ */
+
+//-------------- ACTIVIDAD 1 --------------//
+function entrada(){
+    return prompt("INGRESAR DATO");
 }
 
-//funcion por flecha
-const calcPrecioTotal = precio => {
-    let impuesto = 1.20;
-    let envio = 20;
-    let precioTotal = (precio * impuesto ) + envio;
+function procesamiento(valor){
+    return "LA ENTRADA ES "+valor
+}
+
+function salida(valor){
+    alert(valor);
+}
+
+salida(procesamiento(entrada()));
+//-------------- ACTIVIDAD 2 --------------//
+function redondeo(valor){
+    return Math.round(valor)
+}
+for (let index = 0; index < 5; index++) {
+    let entrada = prompt("INGRESAR NUMERO");
+    alert(redondeo(entrada));
+}
+//-------------- ACTIVIDAD 3 --------------//
+function impuesto(precio, porcentaje){
+    return precio + ((precio * porcentaje)/100)
+}
+for (let index = 0; index < 5; index++) {
+    let resultado = impuesto(parseFloat(prompt("INGRESAR PRECIO")),parseFloat(prompt("INGRESAR %")));
+    alert(resultado);
+}
+
+//-------------- ACTIVIDAD 4 --------------//
+const COTIZACION_DOLAR = 150;
+const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
+const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
+let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR ");
+let valor = prompt("VALOR");
+switch (seleccion) {
+    case "1":
+        alert(cotizarPesos(valor));
+        break;
+    case "2":
+        alert(cotizarDolar(valor));
+        break;
+    default:
+        break;
+}
+//-------------- ACTIVIDAD 5 --------------//
+function validacion(cadena){
+    return cadena != '';
+}
+let entrada =  prompt("INGRESAR CADENA");
+while (entrada != 'ESC') {
+    alert(validacion(entrada));
+    entrada = prompt("INGRESAR CADENA");
 }
