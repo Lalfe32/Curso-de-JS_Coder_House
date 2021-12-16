e20ab2d62d65692481f2a1247594e03c212b9a11
Leandro Lopez <lopezlh982@gmail.com>
2021-12-15 23:29:27 -0300

    realice actividad practica

--- clase-4/JS/index.js
+++ clase-4/JS/index.js
@@ -283,59 +283,105 @@
  * actividades practicas
  */
 
+/**
+ * Crear una funcion anónima que cumpla con las siguientes condiciones:
+ * 1- Si se pasa un número, retornar el valor multiplicado por 3.
+ * 2- Si se para un string, retornar el string "ARR!".
+ * 3- Si se pasa cualquier otro tipo, retornar el mismo valor.
+ */
+
+//Para que una funcion sea anonima se le debe para primero la variable
+// (const / let) variable = function (){ intruccion }
+
+// const entrada = function (value) {
+//     if(typeof value === 'number'){
+//         return value * 3;
+//     }else if (typeof value === 'string'){
+//         return 'ARR!';
+//     }else{
+//         return value;
+//     }
+// }
+
+// const entrada2 = (value) => {
+//     if(typeof value === 'number'){
+//         return value * 3;
+//     }else if (typeof value === 'string'){
+//         return 'ARR!';
+//     }else{
+//         return value;
+//     }
+// }
+// console.log(entrada(true));
+// console.log(entrada2(24));
+
+const win = (value) => {
+    if(typeof value === 'number'){
+        return value - 4;
+    }else if(typeof value === 'string'){
+        return 'hola, bienvenido';
+    }else{
+        return value;
+    }
+}
+console.log(win(10))
+
+
+
+
 //-------------- ACTIVIDAD 1 --------------//
-function entrada(){
-    return prompt("INGRESAR DATO");
-}
+// function entrada(){
+//     return prompt("INGRESAR DATO");
+// }
 
-function procesamiento(valor){
-    return "LA ENTRADA ES "+valor
-}
+// function procesamiento(valor){
+//     return "LA ENTRADA ES "+valor
+// }
 
-function salida(valor){
-    alert(valor);
-}
+// function salida(valor){
+//     alert(valor);
+// }
 
-salida(procesamiento(entrada()));
+// salida(procesamiento(entrada()));
 //-------------- ACTIVIDAD 2 --------------//
-function redondeo(valor){
-    return Math.round(valor)
-}
-for (let index = 0; index < 5; index++) {
-    let entrada = prompt("INGRESAR NUMERO");
-    alert(redondeo(entrada));
-}
-//-------------- ACTIVIDAD 3 --------------//
-function impuesto(precio, porcentaje){
-    return precio + ((precio * porcentaje)/100)
-}
-for (let index = 0; index < 5; index++) {
-    let resultado = impuesto(parseFloat(prompt("INGRESAR PRECIO")),parseFloat(prompt("INGRESAR %")));
-    alert(resultado);
-}
+// function redondeo(valor){
+//     return Math.round(valor)
+// }
+// for (let index = 0; index < 5; index++) {
+//     let entrada = prompt("INGRESAR NUMERO");
+//     alert(redondeo(entrada));
+// }
+// //-------------- ACTIVIDAD 3 --------------//
+// function impuesto(precio, porcentaje){
+//     return precio + ((precio * porcentaje)/100)
+// }
+// for (let index = 0; index < 5; index++) {
+//     let resultado = impuesto(parseFloat(prompt("INGRESAR PRECIO")),parseFloat(prompt("INGRESAR %")));
+//     alert(resultado);
+// }
 
 //-------------- ACTIVIDAD 4 --------------//
-const COTIZACION_DOLAR = 150;
-const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
-const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
-let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR ");
-let valor = prompt("VALOR");
-switch (seleccion) {
-    case "1":
-        alert(cotizarPesos(valor));
-        break;
-    case "2":
-        alert(cotizarDolar(valor));
-        break;
-    default:
-        break;
-}
+// const COTIZACION_DOLAR = 150;
+// const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
+// const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
+// let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR ");
+// let valor = prompt("VALOR");
+// switch (seleccion) {
+//     case "1":
+//         alert(cotizarPesos(valor));
+//         break;
+//     case "2":
+//         alert(cotizarDolar(valor));
+//         break;
+//     default:
+//         break;
+// }
 //-------------- ACTIVIDAD 5 --------------//
-function validacion(cadena){
-    return cadena != '';
-}
-let entrada =  prompt("INGRESAR CADENA");
-while (entrada != 'ESC') {
-    alert(validacion(entrada));
-    entrada = prompt("INGRESAR CADENA");
-}
+// function validacion(cadena){
+//     return cadena != '';
+// }
+// let entrada =  prompt("INGRESAR CADENA");
+// while (entrada != 'ESC') {
+//     alert(validacion(entrada));
+//     entrada = prompt("INGRESAR CADENA");
+// }
